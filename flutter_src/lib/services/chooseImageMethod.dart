1ff6722802ebce0f699445f35160eb36c8dd4c06
Brymer-Meneses <meneses.brymer@gmail.com>
2021-02-21 20:45:20 +0800

    Start building the app

---
+++ flutter_src/lib/services/chooseImageMethod.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:relative_scale/relative_scale.dart';
+
+class ChooseImageMethod extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RelativeBuilder(builder: (context, screenHeight, screenWidth, sy, sx) {
+      return Column(children: <Widget>[
+        Container(alignment: ,)
+      ],)
+    });
+  }
+}
